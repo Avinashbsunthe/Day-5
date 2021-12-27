ee6887464f927322ffa938a377670524c3f4df02
Avinashbsunthe <96405692+Avinashbsunthe@users.noreply.github.com>
2021-12-27 08:23:36 +0530

    Add files via upload

---
+++ CD/src/com/assignment/cd/CD.java
@@ -0,0 +1,18 @@
+package com.assignment.cd;
+
+public class CD {
+	String singerName;
+	String title;
+	
+	public CD(String singerName, String title) {
+		super();
+		this.singerName = singerName;
+		this.title = title;
+	}
+
+	@Override
+	public String toString() {
+		return "CD singerName=" + singerName +"\t" +"         CD title=" + title ;
+	}	
+}
+
